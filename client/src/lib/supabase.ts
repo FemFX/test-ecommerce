e1b3a37b05629b087952c929b9e47fccb082dcb7
Иван Гнедько <ignedko34@gmail.com>
2024-03-30 18:09:28 -0700

    refactoring

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,13 +1,5 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-import { createBrowserClient } from "@supabase/ssr";
-
-// export function createClient() {
-//   return createBrowserClient(
-//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-//   );
-// }
 function createSupabaseClient(): SupabaseClient {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || "",
